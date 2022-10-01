d21f3963ba640ecd1aaa11df844c3da01f4a5b44
Bascy <basschouten65@gmail.com>
2022-10-01 13:34:14 +0200

    Added examples, corrected and enhanced Readme

--- README.md
+++ README.md
@@ -8,51 +8,64 @@ https://www.enocean.com/en/products
 Hardware supported:
 * Tested on an ESP32wroom
 * EnOcean PTM215B selfpowered BLE switch
+* EnOcean EMDCB IR sensor
+* EnOcean STM550B Multi sensor devices
 
 
 ## Development
 This library is work in progress and currently includes:
-* Register a PTM switch by BLE address (**"MAC Address"**), specifying a 32 character **Security Key** and seperate **NodeIds** for either rocker A and B
-or specify separate **NodeIds** for the four buttons A0, A1, B0 and B1
-* Task based detection of `Pushed`, `Repeat`, `ReleaseShort` and `ReleaseLong` events
+* Register an EnOcean device BLE address (**"MAC Address"**), specifying a 32 character **Security Key**
 * Security checks on payload length, source address, GAP AD Type and security key encryption using AES128
 * Dispatch events received through BLE to external event handler
-* Dispatch `Repeat` events to external event handler every 1000ms, if button is pushed and held down
+* Task based detection of `Pushed`, `Repeat`, `ReleaseShort` and `ReleaseLong` events for PTM215 devices
+* Dispatch `Repeat` events to external event handler every 500ms, if a PTM215 button is pushed and held down
 
-All classes are defined within the namespace `EnOcean`.
+All classes and types are defined within the namespace `EnOcean`.
 
-Current implementation for PTM215b switches is based on `PTM_215B_User_Manual_v2.1.pdf` included in the `documentation` folder
-
+Current implementation for PTM215b switches is based on [](documentation%5CPTM_215B_User_Manual_v2.1.pdf) included in the `documentation` folder
 
 ## PTM215 Events
 `Pushed` event is generated when a PTM215 button is pushed.
 If the button is held down, `Repeat` events will be generated every 500 ms after an initial delay.
 When the button is released without generating `Repeat` events, then a `ReleaseShort` event is generated,
-otherwise a `ReleaseLong` event is generated.
+otherwise a `ReleaseLong` event is generated on release.
 
 ## Usage for PTM215 switches
 
-* Add dependency to `I-Connect/EnOcean` to your project
-* Add `#include "EnOceanBLEScanner.h"`
-* Create an instance of the `EnOcean::BLEScanner` class
-* Define a class implementing `EnOcean::PTM215EventHandler` and create an instance
+* Add dependency to `i-connect/EnOceanBleDevices` to your project
+* Add `#include "BleScanner.h"'
+* Add `#include "EnOceanBLEScannerSubscriber.h"'
+* Create an instance of the `BLEScanner::Scanner` and the `EnOcean::BLEScannerSubscriber` class
+* Define a class implementing the `EnOcean::PTM215EventHandler` interface and create an instance of that class
 * Initialize BLEDevice
-* Register switches with `BLEAddress`, 32 character `SecurityKey`, **NodeIds** and a pointer to the eventhandler
+* Register `BLEScannerSubscriber` with the `BLEScanner`
+* Register with the BLEScannerSubsacriber the switches with `BLEAddress`, 32 character `SecurityKey`, **NodeIds** and a pointer to the eventhandler 
 * Call `bleScanner.initialize()`
+* Repeatedly call `bleScanner.update()` in the `loop()` method
 
 On every valid switch event from a registered BLEAddress, a call will be made to the `handleEvent()` method of the eventHandler registered with the switch with a `PTM215b::SwitchEvent` as parameter.
 
-The example `basicWthConfigFile.cpp` file contains an example of how to use a `.json` config file to define the properties of the switches used, using the ArduinoJson library
+
+## EMDCB and STM550 Events
+As both the EMDCB and the STM550 devices work in the exactly the same way, they use the same implementation called 'DataEvent...'
+Events received from these devices are passed through to the EventHandler registered with the device. The event contains the payload of the received advertisement, containing the device and a `std::vector` with `EnOcean::Parameter` which contains the parameter type and value received in the advertisement
+
+## Usage for EMDCB / STM550 devices
+
+* Add dependency to `i-connect/EnOceanBleDevices` to your project
+* Add `#include "BleScanner.h"'
+* Add `#include "EnOceanBLEScannerSubscriber.h"'
+* Create an instance of the `BLEScanner::Scanner` and the `EnOcean::BLEScannerSubscriber` class
+* Define an evenhandler class implementing the `EnOcean::DataEventHandler` interface and create an instance of that class
+* Initialize BLEDevice
+* Register `BLEScannerSubscriber` with the `BLEScanner`
+* Register with the BLEScannerSubsacriber the devices with `BLEAddress`, 32 character `SecurityKey`, **NodeIds** and a pointer to your eventhandler 
+* Call `bleScanner.initialize()`
+* Repeatedly call `bleScanner.update()` in the `loop()` method
 
 
-##Debugging
-If `DEBUG_ENOCEAN` is defined, log messages will be output to Serial for debugging
+## Debugging
+If `DEBUG_ENOCEAN` is defined in main or platformio, log messages will be output to Serial for debugging
 
-## Todo
-Future implementations might include:
-* Implement STM550 multiswitch and EMDCB motion detector devices
-* Handling events with multiple buttons pushed or held
-* Make timing of Repeat events configurable per rocker
-* Implement handling optional data in DataPayload
 
 
---
+++ examples/testSignatureAndEvents.h
@@ -0,0 +1,185 @@
+#include "Arduino.h"
+#include "BleScanner.h"
+#include "EnOceanBLEScannerSubscriber.h"
+#include "MultiData/DataEventAdapter.h"
+#include "PTM215/PTM215EventAdapter.h"
+#include "EnOceanUtils.h"
+
+#define PTM_BLE_ADDRESS "E2:15:00:00:D4:AD"
+#define PTM_SECURITY_KEY "8B49C1B3C62DAE598DCFD4C1AFB1956A"
+
+#define EMDCB_BLE_ADDRESS "E5:00:00:00:0C:79"
+#define EMDCB_SECURITY_KEY "31262A904F6D7B4E02DC637A33E762BF"
+
+#define STM550_BLE_ADDRESS "E5:00:10:00:15:D0"
+#define STM550_SECURITY_KEY "F58AFD8719BAB1600B2CCCD06FAE1A98"
+
+class PTMHandler : public EnOcean::PTM215EventHandler {
+  public:
+    PTMHandler(const uint8_t id) : EnOcean::PTM215EventHandler(id) {};
+    virtual ~PTMHandler() {};
+
+    void handleEvent(const EnOcean::PTM215Event& event) override {
+      log_i("Handling PTM Event by node %d", getId());
+      log_i("DeviceAddress: %s", event.device->address.toString().c_str());
+      log_i("Event: ReferenceId: %d button %d type %d", event.referenceId, event.button, event.eventType);
+    }
+};
+
+class EMDCBHandler : public EnOcean::DataEventHandler {
+  public:
+    EMDCBHandler(const uint8_t id) : EnOcean::DataEventHandler(id) {};
+    virtual ~EMDCBHandler() {};
+
+    void handleEvent(const EnOcean::DataEvent& event) override {
+      log_i("Handling EMDCB Event by node %d", getId());
+      log_i("DeviceAddress: %s", event.device->address.toString().c_str());
+      log_i("Parameters:");
+      log_i("\tType \tSize Value");
+      log_i("-----------------------------");
+      for (auto const& parameter : event.parameters) {
+        char b[5];
+        log_i("\t0x%02x \t   %d %s", parameter.type, parameter.size, EnOcean::printBuffer((byte*)&parameter.value, parameter.size, false, "", b));
+      }
+    }
+};
+
+class STM550Handler : public EnOcean::DataEventHandler {
+  public:
+    STM550Handler(const uint8_t id) : EnOcean::DataEventHandler(id) {};
+    virtual ~STM550Handler() {};
+
+    void handleEvent(const EnOcean::DataEvent& event) override {
+      log_i("Handling STM550B Event by node %d", getId());
+      log_i("DeviceAddress: %s", event.device->address.toString().c_str());
+      log_i("Parameters:");
+      log_i("\tType \tSize Value");
+      log_i("-----------------------------");
+      for (auto const& parameter : event.parameters) {
+        char b[5];
+        log_i("\t0x%02x \t   %d %s", parameter.type, parameter.size, EnOcean::printBuffer((byte*)&parameter.value, parameter.size, false, "", b));
+      }
+    }
+};
+
+BleScanner::Scanner scanner;
+EnOcean::BLEScannerSubscriber scannerSubscriber;
+
+void testEMDCBSignature() {
+  EnOcean::DataEventAdapter adapter;
+
+  EnOcean::Device device;
+  device.address = NimBLEAddress("E5:00:00:00:00:C4");
+  uint8_t key[] {0x9E, 0x0D, 0xE9, 0xC2, 0x53, 0x86, 0xB6, 0xC4, 0xF0, 0x70, 0x64, 0x2E, 0x19, 0xE0, 0x36, 0x80};
+  memcpy(device.securityKey, key, 16);
+  EnOcean::Payload payload;
+  payload.len = 0x15;
+  payload.type = 0xFF;
+  payload.payloadType = EnOcean::PayloadType::Data;
+  payload.manufacturerId[0] = 0xDA;
+  payload.manufacturerId[1] = 0x03;
+  payload.sequenceCounter = 0x0001E257;
+  uint8_t data[] {0x02, 0xaa, 0x44, 0xd6, 0x00, 0x45, 0x35, 0x00, 0x20, 0x02};
+  memset(payload.data.raw, 0x00, sizeof(payload.data.raw));
+  memcpy(payload.data.raw, data, sizeof(data));
+
+  uint8_t signature[] {0xc8, 0xcc, 0x57, 0x12};
+  memcpy(payload.data.signature, signature, 4);
+
+  // EnOcean::printBuffer((byte*)&payload, sizeof(payload), false, "Test Payload");
+  log_i("EMDCB Test Security key %s", scannerSubscriber.securityKeyValid(device, payload) ? "valid" : "NOT valid");
+}
+
+void testSTM550Signature() {
+  EnOcean::DataEventAdapter adapter;
+
+  EnOcean::Device device;
+  device.address = NimBLEAddress("E5:00:77:01:00:00");
+  uint8_t key[] {0x4D, 0xB3, 0x4D, 0xB3, 0x07, 0x0E, 0xFC, 0x67, 0x13, 0xFE, 0x39, 0xE1, 0x3C, 0xF3, 0xC9, 0x39};
+  memcpy(device.securityKey, key, 16);
+  EnOcean::Payload payload;
+  payload.len = 0x1C;
+  payload.type = 0xFF;
+  payload.payloadType = EnOcean::PayloadType::Data;
+  payload.manufacturerId[0] = 0xDA;
+  payload.manufacturerId[1] = 0x03;
+  payload.sequenceCounter = 0x00027FB;
+  uint8_t data[] {0x02, 0x68, 0x45, 0x8D, 0x01, 0x40, 0x42, 0x09, 0x06, 0x57, 0x8A, 0xF7, 0x91, 0xE6, 0x5E, 0x23, 0x01 };
+  memset(payload.data.raw, 0x00, sizeof(payload.data.raw));
+  memcpy(payload.data.raw, data, sizeof(data));
+
+  uint8_t signature[] {0x0F, 0x01, 0x57, 0xD3};
+  memcpy(payload.data.signature, signature, 4);
+
+  // EnOcean::printBuffer((byte*)&payload, sizeof(payload), false, "Test Payload");
+  log_i("STM550 Test Security key %s", scannerSubscriber.securityKeyValid(device, payload) ? "valid" : "NOT valid");
+}
+
+void testPTMSignature() {
+  EnOcean::Device device;
+  EnOcean::PTM215EventAdapter ptmAdapter;
+
+  device.address = NimBLEAddress("E2:15:00:00:19:B8");
+  uint8_t key2[] {0x3D, 0xDA, 0x31, 0xAD, 0x44, 0x76, 0x7A, 0xE3, 0xCE, 0x56, 0xDC, 0xE2, 0xB3, 0xCE, 0x2A, 0xBB};
+  memcpy(device.securityKey, key2, 16);
+  EnOcean::Payload payload;
+  memset(&payload, 0x00, sizeof(payload));
+  payload.len = 0x0C;
+  payload.type = 0xFF;
+  payload.payloadType = EnOcean::PayloadType::Data;
+  payload.manufacturerId[0] = 0xDA;
+  payload.manufacturerId[1] = 0x03;
+  payload.sequenceCounter = 0x0000045D;
+  uint8_t data2[] = {0x11};
+  memcpy(payload.data.raw, data2, sizeof(data2));
+
+  uint8_t signature2[] = {0xB2, 0xFA, 0x88, 0xFF};
+  memcpy(payload.data.signature, signature2, 4);
+
+  // EnOcean::printBuffer((byte*)&payload, sizeof(payload), false, "Payload");
+
+  log_i("PTM215 Test Security key %s", scannerSubscriber.securityKeyValid(device, payload) ? "valid" : "NOT valid");
+}
+
+PTMHandler* ptmHandler;
+PTMHandler* handler2;
+EMDCBHandler* emdcbHandler;
+STM550Handler* stmHandler;
+
+void setup() {
+  Serial.begin(115200);
+  log_i("Starting EnOcean BLE Example application...");
+
+  ptmHandler = new PTMHandler(1);
+  handler2 = new PTMHandler(2);
+  emdcbHandler = new EMDCBHandler(3);
+  stmHandler = new STM550Handler(4);
+
+  scanner.setScanDuration(1);
+  scanner.subscribe(&scannerSubscriber);
+
+  NimBLEDevice::init("ESP32_client");
+
+  scanner.initialize();
+
+  log_d("Adding devices");
+  // register handler for A0 and B0 buttons using pointer to handler
+  scannerSubscriber.registerPTM215Device(PTM_BLE_ADDRESS, PTM_SECURITY_KEY, ptmHandler, true, false, true, false, 15);
+  // register handler for A1, B0 and B1 buttons, using nodeId of handler
+  scannerSubscriber.registerPTM215Device(PTM_BLE_ADDRESS, PTM_SECURITY_KEY, 2, false, true, true, true, 37);
+
+  scannerSubscriber.registerDataDevice(EMDCB_BLE_ADDRESS, EMDCB_SECURITY_KEY, emdcbHandler);
+
+  scannerSubscriber.registerDataDevice(STM550_BLE_ADDRESS, STM550_SECURITY_KEY, stmHandler);
+  log_i("Initialization done");
+  log_i("===========================================");
+
+  testPTMSignature();
+  testEMDCBSignature();
+  testSTM550Signature();
+}
+
+void loop() {
+  scanner.update();
+  delay(100);
+}
